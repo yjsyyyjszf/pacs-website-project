dece37e19651f4ecb0bc0dbf55f67cc7eeabe9f6
Lucas <lucasrasecc@gmail.com>
2020-05-20 10:21:17 -0300

    Create image component

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -3,12 +3,13 @@ import { Container, Img } from './styles';
 
 interface Props {
     source: string,
-    opaque?: boolean
+    opaque?: boolean,
+    align: "left"|"right"|"center"
 }
 
-const Image: React.FC<Props> = ({ source, opaque }) => {
+const Image: React.FC<Props> = ({ source, opaque, align }) => {
     return (
-        <Container>
+        <Container align={align}>
             <Img src={source} opaque={!!opaque} />
         </Container>
     )
--- src/components/Image/styles.tsx
+++ src/components/Image/styles.tsx
@@ -1,10 +1,35 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-
+const checkAlign = (align: string) => {
+    switch (align) {
+        case 'right':
+            return 'flex-end';
+        case 'left':
+            return 'flex-start';
+        case 'center':
+            return 'center';
+    }
+}
+export const Container = styled.div<{align: string}>`
+    display: flex;
+    flex-direction: row;
+    justify-content: ${({ align }) => checkAlign(align)};
+    padding: 1em;
+    background: white; 
 `;
 
 export const Img = styled.img<{opaque: boolean}>`
     width: 22rem;
-    opacity: ${({ opaque }) => opaque ? 0.3 : 1 }
+    opacity: ${({ opaque }) => opaque ? 0.3 : 1 };  
 `; 
+
+// function troca(align: string) {
+//     switch (align) {
+//         case 'right':
+//             return 'flex-end';
+//         case 'left':
+//             return 'flex-start';
+//         case 'center':
+//             return 'center';
+//     }
+// }
