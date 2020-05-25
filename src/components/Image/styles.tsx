083e97b808c12398fa928c890732d630804eecaa
fuhr3r <gabriel.ferreira019@gmail.com>
2020-05-25 13:03:23 -0300

    Update Image to fill wrapper container

--- src/components/Image/styles.tsx
+++ src/components/Image/styles.tsx
@@ -19,17 +19,7 @@ export const Container = styled.div<{align: string}>`
 `;
 
 export const Img = styled.img<{opaque: boolean}>`
-    width: 22rem;
+    width: 100%;
+    height: auto;
     opacity: ${({ opaque }) => opaque ? 0.3 : 1 };  
-`; 
-
-// function troca(align: string) {
-//     switch (align) {
-//         case 'right':
-//             return 'flex-end';
-//         case 'left':
-//             return 'flex-start';
-//         case 'center':
-//             return 'center';
-//     }
-// }
+`;
