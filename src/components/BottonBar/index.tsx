faa79fecdd366d55e0def663f167d92ad799b838
Lucas <lucasrasecc@gmail.com>
2020-06-01 11:24:06 -0300

    Create Botton Bar

---
+++ src/components/BottonBar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Text from '../Text';
+import Image from '../Image';
+import { Container, TitleContainer, InfoContent, TextWrapper, ContentWrapper } from './styles';
+import Title from '../Title';
+import logo from '../../assets/images/logo.svg';
+
+
+const Content: React.FC = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Content;
